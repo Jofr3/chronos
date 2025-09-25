c12aa07d90122bac75607cabcbb5be86b8c7119f
Jofr3 <jofrescari@gmail.com>
2025-09-25 18:12:49 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "chronos";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+
+            vue-language-server
+            typescript-language-server
+          ];
+
+          shellHook = ''
+          '';
+        };
+      });
+}
