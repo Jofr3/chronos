44ab8805738a024596e70daf5d9fa8c4760640bb
Jofr3 <jofrescari@gmail.com>
2026-01-07 19:15:04 +0100

    configure direnv, dotenv & .envrc

--- flake.nix
+++ flake.nix
@@ -13,26 +13,9 @@
         };
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ bun cacert ];
+          buildInputs = with pkgs; [ bun cacert dotenv-cli ];
 
           shellHook = ''
-            # Load .env variables
-            VARS_TO_LOAD=(
-              "CONTEXT7_API_KEY"
-            )
-            ENV_FILE=".env"
-
-            if [ -f "$ENV_FILE" ]; then
-                for var_name in "''${VARS_TO_LOAD[@]}"; do
-                    match=$(grep "^$var_name=" "$ENV_FILE")
-                    if [ -n "$match" ]; then
-                        val="''${match#*=}"
-                        val=$(echo "$val" | sed -e 's/^"//' -e 's/"$//' -e "s/^'//" -e "s/'$//")
-                        export "$var_name"="$val"
-                    fi
-                done
-            fi
-
             # SSL Certificate
             export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
           '';
